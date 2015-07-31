f78563f2874e81c8141d5dd1145d5c3d6d47056a
Jack Boberg <jack.boberg@gmail.com>
2015-07-30 23:17:14 -0400

    Turn on most "Possible Errors"

--- eslintrc.js
+++ eslintrc.js
@@ -55,35 +55,35 @@ module.exports = {
   "rules": {
     ////////// Possible Errors //////////
 
-    "comma-dangle": 0,             // disallow trailing commas in object literals
-    "no-cond-assign": 0,           // disallow assignment in conditional expressions
-    "no-console": 0,               // disallow use of console (off by default in the node environment)
-    "no-constant-condition": 0,    // disallow use of constant expressions in conditions
-    "no-control-regex": 0,         // disallow control characters in regular expressions
-    "no-debugger": 0,              // disallow use of debugger
-    "no-dupe-args": 0,             // disallow duplicate arguments in functions
-    "no-dupe-keys": 0,             // disallow duplicate keys when creating object literals
-    "no-duplicate-case": 0,        // disallow a duplicate case label
-    "no-empty-character-class": 0, // disallow the use of empty character classes in regular expressions
-    "no-empty": 0,                 // disallow empty statements
-    "no-ex-assign": 0,             // disallow assigning to the exception in a catch block
-    "no-extra-boolean-cast": 0,    // disallow double-negation boolean casts in a boolean context
-    "no-extra-parens": 0,          // disallow unnecessary parentheses (off by default)
-    "no-extra-semi": 0,            // disallow unnecessary semicolons
-    "no-func-assign": 0,           // disallow overwriting functions written as function declarations
-    "no-inner-declarations": 0,    // disallow function or variable declarations in nested blocks
-    "no-invalid-regexp": 0,        // disallow invalid regular expression strings in the RegExp constructor
-    "no-irregular-whitespace": 0,  // disallow irregular whitespace outside of strings and comments
-    "no-negated-in-lhs": 0,        // disallow negation of the left operand of an in expression
-    "no-obj-calls": 0,             // disallow the use of object properties of the global object (Math and JSON) as functions
-    "no-regex-spaces": 0,          // disallow multiple spaces in a regular expression literal
-    "no-reserved-keys": 0,         // disallow reserved words being used as object literal keys (off by default)
-    "no-sparse-arrays": 0,         // disallow sparse arrays
-    "no-unreachable": 0,           // disallow unreachable statements after a return, throw, continue, or break statement
-    "use-isnan": 0,                // disallow comparisons with the value NaN
-    "valid-jsdoc": 0,              // Ensure JSDoc comments are valid (off by default)
-    "valid-typeof": 0,             // Ensure that the results of typeof are compared against a valid string
-    "no-unexpected-multiline": 0,  // Avoid code that looks like two expressions but is actually one (off by default)
+    "comma-dangle": [2, "never"],               // disallow trailing commas in object literals
+    "no-cond-assign": 2,                        // disallow assignment in conditional expressions
+    "no-console": 1,                            // disallow use of console (off by default in the node environment)
+    "no-constant-condition": 2,                 // disallow use of constant expressions in conditions
+    "no-control-regex": 2,                      // disallow control characters in regular expressions
+    "no-debugger": 2,                           // disallow use of debugger
+    "no-dupe-args": 2,                          // disallow duplicate arguments in functions
+    "no-dupe-keys": 2,                          // disallow duplicate keys when creating object literals
+    "no-duplicate-case": 2,                     // disallow a duplicate case label
+    "no-empty-character-class": 2,              // disallow the use of empty character classes in regular expressions
+    "no-empty": 2,                              // disallow empty statements
+    "no-ex-assign": 2,                          // disallow assigning to the exception in a catch block
+    "no-extra-boolean-cast": 2,                 // disallow double-negation boolean casts in a boolean context
+    "no-extra-parens": 2,                       // disallow unnecessary parentheses (off by default)
+    "no-extra-semi": 2,                         // disallow unnecessary semicolons
+    "no-func-assign": 2,                        // disallow overwriting functions written as function declarations
+    "no-inner-declarations": [2, "functions"],  // disallow function or variable declarations in nested blocks
+    "no-invalid-regexp": 2,                     // disallow invalid regular expression strings in the RegExp constructor
+    "no-irregular-whitespace": 2,               // disallow irregular whitespace outside of strings and comments
+    "no-negated-in-lhs": 2,                     // disallow negation of the left operand of an in expression
+    "no-obj-calls": 2,                          // disallow the use of object properties of the global object (Math and JSON) as functions
+    "no-regex-spaces": 2,                       // disallow multiple spaces in a regular expression literal
+    "no-reserved-keys": 2,                      // disallow reserved words being used as object literal keys (off by default)
+    "no-sparse-arrays": 2,                      // disallow sparse arrays
+    "no-unreachable": 2,                        // disallow unreachable statements after a return, throw, continue, or break statement
+    "use-isnan": 2,                             // disallow comparisons with the value NaN
+    "valid-jsdoc": 0,                           // Ensure JSDoc comments are valid (off by default)
+    "valid-typeof": 2,                          // Ensure that the results of typeof are compared against a valid string
+    "no-unexpected-multiline": 2,               // Avoid code that looks like two expressions but is actually one (off by default)
 
 
     ////////// Best Practices //////////
