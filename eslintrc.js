d2edb6cdb9eb60379572c3f80d89d6842b803116
Jack Boberg <jack.boberg@gmail.com>
2015-07-30 22:42:36 -0400

    Update available env options and set `amd` to `false`

--- eslintrc.js
+++ eslintrc.js
@@ -29,14 +29,19 @@ module.exports = {
 
   "env": {
     "browser": false,     // browser global variables
+    "worker": false,      // web workers global variables.
     "node": true,         // Node.js global variables and Node.js-specific rules
-    "amd": true,          // defines require() and define() as global variables as per the amd spec
+    "amd": false,         // defines require() and define() as global variables as per the amd spec
     "mocha": false,       // adds all of the Mocha testing global variables
     "jasmine": false,     // adds all of the Jasmine testing global variables for version 1.3 and 2.0
     "phantomjs": false,   // phantomjs global variables
     "jquery": false,      // jquery global variables
     "prototypejs": false, // prototypejs global variables
     "shelljs": false,     // shelljs global variables
+    "meteor": false,      // meteor global variables.
+    "mongo": false,       // mongo global variables.
+    "applescript": false, // applescript global variables.
+    "es6": false          // enable all ECMAScript 6 features except for modules.
   },
 
   "globals": {
