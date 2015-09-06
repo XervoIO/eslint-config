ae608a7e56cc910da640bd0df6549c401814caf5
Jack Boberg <jack.boberg@gmail.com>
2015-09-05 22:21:36 -0400

    Disallow use of undefined when initializing variables

--- eslintrc.js
+++ eslintrc.js
@@ -159,7 +159,7 @@ module.exports = {
     "no-shadow": 0,                   // disallow declaration of variables already declared in the outer scope
     "no-shadow-restricted-names": 0,  // disallow shadowing of names such as arguments
     "no-undef": 2,                    // disallow use of undeclared variables unless mentioned in a /*global */ block
-    "no-undef-init": 0,               // disallow use of undefined when initializing variables
+    "no-undef-init": 2,               // disallow use of undefined when initializing variables
     "no-undefined": 0,                // disallow use of undefined variable (off by default)
     "no-unused-vars": 0,              // disallow declaration of variables that are not used in the code
     "no-use-before-define": 0,        // disallow use of variables before they are defined
