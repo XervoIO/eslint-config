a914a1da4d37b010c52221b11fe694b98389ab73
Jack Boberg <jack.boberg@gmail.com>
2015-09-05 22:22:41 -0400

    Disallow use of undefined variable

--- eslintrc.js
+++ eslintrc.js
@@ -160,7 +160,7 @@ module.exports = {
     "no-shadow-restricted-names": 0,  // disallow shadowing of names such as arguments
     "no-undef": 2,                    // disallow use of undeclared variables unless mentioned in a /*global */ block
     "no-undef-init": 2,               // disallow use of undefined when initializing variables
-    "no-undefined": 0,                // disallow use of undefined variable (off by default)
+    "no-undefined": 2,                // disallow use of undefined variable (off by default)
     "no-unused-vars": 0,              // disallow declaration of variables that are not used in the code
     "no-use-before-define": 0,        // disallow use of variables before they are defined
 
