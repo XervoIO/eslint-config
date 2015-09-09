33834aebe9b8f6bb5e57288ea3a2c44358eaecd0
Jack Boberg <jack.boberg@gmail.com>
2015-09-08 23:34:09 -0400

    Restrict what can be thrown as an exception

--- eslintrc.js
+++ eslintrc.js
@@ -135,7 +135,7 @@ module.exports = {
     "no-script-url": 2,         // disallow use of javascript: urls
     "no-self-compare": 2,       // disallow comparisons where both sides are exactly the same (off by default)
     "no-sequences": 0,          // disallow use of comma operator
-    "no-throw-literal": 0,      // restrict what can be thrown as an exception (off by default)
+    "no-throw-literal": 2,      // restrict what can be thrown as an exception (off by default)
     "no-unused-expressions": 0, // disallow usage of expressions in statement position
     "no-void": 0,               // disallow use of void operator (off by default)
     "no-warning-comments": 0,   // disallow usage of configurable warning terms in comments, e.g. TODO or FIXME (off by default)
