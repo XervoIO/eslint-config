f28ee9f65f14ad7cf9f997d4fc01d33fedbe515c
Jack Boberg <jack.boberg@gmail.com>
2015-09-08 23:22:35 -0400

    Disallows creating new instances of String, Number, and Boolean

--- eslintrc.js
+++ eslintrc.js
@@ -123,7 +123,7 @@ module.exports = {
     "no-multi-str": 2,          // disallow use of multiline strings
     "no-native-reassign": 2,    // disallow reassignments of native objects
     "no-new-func": 0,           // disallow use of new operator for Function object
-    "no-new-wrappers": 0,       // disallows creating new instances of String, Number, and Boolean
+    "no-new-wrappers": 2,       // disallows creating new instances of String, Number, and Boolean
     "no-new": 0,                // disallow use of new operator when not part of the assignment or comparison
     "no-octal-escape": 0,       // disallow use of octal escape sequences in string literals, such as var foo = "Copyright \251";
     "no-octal": 0,              // disallow use of octal literals
