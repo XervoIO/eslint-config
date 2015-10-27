85d620d3f2dd2038f5a0a7bc9379e333eae91f55
Jack Boberg <jack.boberg@gmail.com>
2015-10-26 22:55:00 -0400

    Add new rules (defaults) and remove old rules

--- eslintrc.js
+++ eslintrc.js
@@ -82,7 +82,6 @@ module.exports = {
     "no-negated-in-lhs": 2,                     // disallow negation of the left operand of an in expression
     "no-obj-calls": 2,                          // disallow the use of object properties of the global object (Math and JSON) as functions
     "no-regex-spaces": 2,                       // disallow multiple spaces in a regular expression literal
-    "no-reserved-keys": 0,                      // disallow reserved words being used as object literal keys (off by default)
     "no-sparse-arrays": 2,                      // disallow sparse arrays
     "no-unreachable": 2,                        // disallow unreachable statements after a return, throw, continue, or break statement
     "use-isnan": 2,                             // disallow comparisons with the value NaN
@@ -108,17 +107,21 @@ module.exports = {
     "no-div-regex": 0,          // disallow division operators explicitly at beginning of regular expression (off by default)
     "no-else-return": 2,        // disallow else after a return in an if (off by default)
     "no-empty-label": 0,        // disallow use of labels for anything other then loops and switches
+    "no-empty-pattern": 0,      // disallow use of empty destructuring patterns
     "no-eq-null": 0,            // disallow comparisons to null without a type-checking operator (off by default)
     "no-eval": 2,               // disallow use of eval()
     "no-extend-native": 0,      // disallow adding to native types
     "no-extra-bind": 2,         // disallow unnecessary function binding
     "no-fallthrough": 2,        // disallow fallthrough of case statements
     "no-floating-decimal": 2,   // disallow the use of leading or trailing decimal points in numeric literals (off by default)
+    "no-implicit-coercion": 0,  // disallow the type conversions with shorter notations
     "no-implied-eval": 2,       // disallow use of eval()-like methods
+    "no-invalid-this": 0,       // disallow this keywords outside of classes or class-like objects
     "no-iterator": 2,           // disallow usage of __iterator__ property
     "no-labels": 0,             // disallow use of labeled statements
     "no-lone-blocks": 2,        // disallow unnecessary nested blocks
     "no-loop-func": 2,          // disallow creation of functions within loops
+    "no-magic-numbers": 0,      // disallow the use of magic numbers
     "no-multi-spaces": 2,       // disallow use of multiple spaces
     "no-multi-str": 2,          // disallow use of multiline strings
     "no-native-reassign": 2,    // disallow reassignments of native objects
@@ -137,6 +140,8 @@ module.exports = {
     "no-sequences": 0,          // disallow use of comma operator
     "no-throw-literal": 2,      // restrict what can be thrown as an exception (off by default)
     "no-unused-expressions": 2, // disallow usage of expressions in statement position
+    "no-useless-call": 0,       // disallow unnecessary .call() and .apply()
+    "no-useless-concat": 0,     // disallow unnecessary concatenation of literals or template literals
     "no-void": 0,               // disallow use of void operator (off by default)
     "no-warning-comments": 0,   // disallow usage of configurable warning terms in comments, e.g. TODO or FIXME (off by default)
     "no-with": 0,               // disallow use of the with statement
@@ -153,6 +158,7 @@ module.exports = {
 
     ////////// Variables //////////
 
+    "init-declarations": 0,           // enforce or disallow variable initializations at definition
     "no-catch-shadow": 0,             // disallow the catch clause parameter name being the same as a variable in the outer scope (off by default in the node environment)
     "no-delete-var": 2,               // disallow deletion of variables
     "no-label-var": 2,                // disallow labels that share a name with a variable
@@ -167,6 +173,8 @@ module.exports = {
 
     ////////// Node.js //////////
 
+    "callback-return": 0,       // enforce return after a callback
+    "global-require": 0,        // enforce require() on top-level module scope
     "handle-callback-err": 2,   // enforces error handling in callbacks (off by default) (on by default in the node environment)
     "no-mixed-requires": 2,     // disallow mixing regular variable and require declarations (off by default) (on by default in the node environment)
     "no-new-require": 2,        // disallow use of new operator with the require function (off by default) (on by default in the node environment)
@@ -179,6 +187,7 @@ module.exports = {
     ////////// Stylistic Issues //////////
 
     "array-bracket-spacing": [2, "never"], // enforce spacing inside array brackets (off by default)
+    "block-spacing": 0,               // disallow or enforce spaces inside of single line blocks (fixable)
     "brace-style": 2,                 // enforce one true brace style (off by default)
     "camelcase": 0,                   // require camel case names
     "comma-spacing": 0,               // enforce spacing before and after comma
@@ -188,7 +197,10 @@ module.exports = {
     "eol-last": 0,                    // enforce newline at the end of file, with no multiple empty lines
     "func-names": 0,                  // require function expressions to have a name (off by default)
     "func-style": 0,                  // enforces use of function declarations or expressions (off by default)
+    "id-length": 0,                   // this option enforces minimum and maximum identifier lengths (variable names, property names etc.)
+    "id-match": 0,                    // require identifiers to match the provided regular expression
     "indent": [2, 2, { "SwitchCase": 1 }], // this option sets a specific tab width for your code (off by default)
+    "jsx-quotes": [0, "prefer-double"], // specify whether double or single quotes should be used in JSX attributes
     "key-spacing": [2, { "beforeColon": false, "afterColon": true }], // enforces spacing between keys and values in object literal properties
     "lines-around-comment": 0,        // enforces empty lines around comments (off by default)
     "linebreak-style": 0,             // disallow mixed 'LF' and 'CRLF' as linebreaks (off by default)
@@ -202,24 +214,30 @@ module.exports = {
     "no-lonely-if": 2,                // disallow if as the only statement in an else block (off by default)
     "no-mixed-spaces-and-tabs": 2,    // disallow mixed spaces and tabs for indentation
     "no-multiple-empty-lines": [2, { max: 1 }], // disallow multiple empty lines (off by default)
+    "no-negated-condition": 0,        // disallow negated conditions
     "no-nested-ternary": 2,           // disallow nested ternary expressions (off by default)
     "no-new-object": 0,               // disallow use of the Object constructor
+    "no-restricted-syntax": 0,        // disallow use of certain syntax in code
     "no-spaced-func": 2,              // disallow space between function identifier and application
     "no-ternary": 0,                  // disallow the use of ternary operators (off by default)
     "no-trailing-spaces": 2,          // disallow trailing whitespace at the end of lines
     "no-underscore-dangle": 2,        // disallow dangling underscores in identifiers
+    "no-unneeded-ternary": 0,         // disallow the use of ternary operators when a simpler alternative exists
+    "object-curly-spacing": [0, "never"], // require or disallow padding inside curly braces (fixable)
     "one-var": 0,                     // allow just one var statement per function (off by default)
     "operator-assignment": 0,         // require assignment operator shorthand where possible or prohibit it entirely (off by default)
     "operator-linebreak": 0,          // enforce operators to be placed before or after line breaks (off by default)
     "padded-blocks": [2, "never"],    // enforce padding within blocks (off by default)
     "quote-props": 0,                 // require quotes around object literal property names (off by default)
     "quotes": [2, "single", "avoid-escape"], // specify whether double or single quotes should be used
+    "require-jsdoc": 0,               // Require JSDoc comment
     "semi-spacing": 0,                // enforce spacing before and after semicolons
     "semi": 2,                        // require or disallow use of semicolons instead of ASI
     "sort-vars": 0,                   // sort variables within the same declaration block (off by default)
     "space-after-keywords": [2, "always"], // require a space after certain keywords (off by default)
     "space-before-blocks": [2, "always"], // require or disallow space before blocks (off by default)
     "space-before-function-paren": [2, { "anonymous": "always", "named": "never" }], // require or disallow space before function opening parenthesis (off by default)
+    "space-before-keywords": 0,       // require a space before certain keywords (fixable)
     "space-in-parens": [2, "never"],  // require or disallow spaces inside parentheses (off by default)
     "space-infix-ops": 2,             // require spaces around operators
     "space-return-throw-case": 2,     // require a space after return, throw, and case
@@ -230,12 +248,22 @@ module.exports = {
 
     ////////// ECMAScript 6 //////////
 
+    "arrow-parens": 0,           // require parens in arrow function arguments
+    "arrow-spacing": 0,          // require space before/after arrow function's arrow (fixable)
     "constructor-super": 0,      // verify super() callings in constructors (off by default)
     "generator-star-spacing": 0, // enforce the spacing around the * in generator functions (off by default)
     "no-this-before-super": 0,   // disallow to use this/super before super() calling in constructors (off by default)
     "no-var": 0,                 // require let or const instead of var (off by default)
     "object-shorthand": 0,       // require method and property shorthand syntax for object literals (off by default)
+    "prefer-arrow-callback": 0,  // suggest using arrow functions as callbacks
     "prefer-const": 1,           // suggest using of const declaration for variables that are never modified after declared (off by default)
+    "prefer-reflect": 0,         // suggest using Reflect methods where applicable
+    "prefer-spread": 0,          // suggest using the spread operator instead of .apply()
+    "prefer-template": 0,        // suggest using template literals instead of strings concatenation
+    "require-yield": 0,          // disallow generator functions that do not have yield
+    "no-class-assign": 0,        // disallow modifying variables of class declarations
+    "no-const-assign": 0,        // disallow modifying variables that are declared using const
+    "no-dupe-class-members": 0,  // disallow duplicate name in class members
 
 
     ////////// Legacy //////////
